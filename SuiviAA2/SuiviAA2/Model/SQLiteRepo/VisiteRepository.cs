068b240c4ad6649d81a22bbeb5e3e885f0fa5dab
zatharan <jg_972@hotmail.com>
2017-05-05 18:39:26 +0200

    connection DB visiteRepo

--- SuiviAA2/SuiviAA2/Model/SQLiteRepo/VisiteRepository.cs
+++ SuiviAA2/SuiviAA2/Model/SQLiteRepo/VisiteRepository.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net;
+using SQLite.Net.Interop;
+using SQLite.Net.Async;
 
 namespace SuiviAA2.Model.SQLiteRepo
 {
-    class VisiteRepository
+    public class VisiteRepository
     {
+        private SQLiteAsyncConnection dbConn;
+
+        public string StatusMessage { get; set; }
+
+        public VisiteRepository(ISQLitePlatform sqlitePlatform, string dbPath)
+        {
+            //initialise la connection à la base SQLite
+            if(dbConn == null)
+            {
+                var connectionFunc = new Func<SQLiteConnectionWithLock>(() =>
+                    new SQLiteConnectionWithLock
+                    (
+                        sqlitePlatform,
+                        new SQLiteConnectionString(dbPath, storeDateTimeAsTicks: false)
+                    ));
+
+                dbConn = new SQLiteAsyncConnection(connectionFunc);
+                dbConn.CreateTableAsync<Visite>(); //Crée la table visite
+            }
+
+        }
+
+        public async Task AddNewVisiteAsync()
+        {
+            //ajouter méthode ajout visite
+        }
     }
 }
