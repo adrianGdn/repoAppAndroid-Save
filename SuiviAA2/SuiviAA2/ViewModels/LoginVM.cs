73b2b6c576502ac72b6ae06d033e0fb156c730e1
zatharan <jg_972@hotmail.com>
2017-05-07 23:29:40 +0200

    connexion OK

--- SuiviAA2/SuiviAA2/ViewModels/LoginVM.cs
+++ SuiviAA2/SuiviAA2/ViewModels/LoginVM.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using static SuiviAA2.ViewModels.BaseVM;
+using System.Security;
+using System.Diagnostics;
 
 namespace SuiviAA2.ViewModels
 {
@@ -22,24 +24,40 @@ namespace SuiviAA2.ViewModels
         #region get/set
         public string Login { get => login; set => login = value; } // Recupère automatiquement la valeur du champ ent_login de la mainpage
         public string Pass { get => pass; set => pass = value; } // Recupère automatiquement la valeur du champ ent_mdp de la mainpage
+        public string shAmdp;
         #endregion
         #region methods
         public async Task Connexion()
         {
-            // url de récupération du json de l'acteur
-            string urlActeur = "http://10.0.0.5/ppe3JoJuAd/gsbAppliFraisV2/webservices/w_visiteur.php" + "?" + "login=" + Login + "&" + "pass=" + Pass;
+            // on met en place un try catch pour déceler toute erreur dans la procédure de connexion
+            try
+            {
+                // url de récupération du json de l'acteur
+                string urlActeur = "http://10.0.0.5/ppe3JoJuAd/gsbAppliFraisV2/webservices/w_visiteur.php" + "?" + "login=" + Login + "&" + "pass=" + Pass;
 
-            //instanciation du client http qui envoi un header json
-            HttpClient clientActeur = new HttpClient();
-            clientActeur.DefaultRequestHeaders.Accept.Clear();
-            clientActeur.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //instanciation du client http qui envoi un header json
+                HttpClient clientActeur = new HttpClient();
+                clientActeur.DefaultRequestHeaders.Accept.Clear();
+                clientActeur.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            //réponse à la requête Http
-            var response = await clientActeur.GetAsync(urlActeur);
-            var json = response.Content.ReadAsStringAsync().Result;
-            var acteurJson = JsonConvert.DeserializeObject<ActeurJson>(json);
+                //réponse à la requête Http
+                var response = await clientActeur.GetAsync(urlActeur);
+                var json = response.Content.ReadAsStringAsync().Result;
+                var acteurJson = JsonConvert.DeserializeObject<ActeurJson>(json);
 
-            App.Current.MainPage = new VisitePage();
+                //on vérifie les informations de connexion du user (ici cela se ait avec oldMdp car pas d'implémentation du SHA1 actuellement en Xamarin, auquel cas nous auions converti le contenu du champ pass en sha1 puis vérification avec le champ mdp de l'acteur)
+                if (acteurJson.Acteur.login == login && acteurJson.Acteur.oldMdp == pass)
+                {
+                    App.Current.MainPage = new VisitePage();
+                }
+            }
+            catch
+            {
+                await App.Current.MainPage.DisplayAlert("Impossible de se connecter", "L'authentification à échouée", "OK");
+                return;
+            }
+            
+
         }
 
         #endregion
